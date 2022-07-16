919e91a0be0faf29b21891abffc054d315e887b7
Nodding <luccacioffi@gmail.com>
2022-07-16 17:53:39 -0400

    Player and Character name getting

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 // First Go program
 package main
   
-import "fmt"
-  
+import (
+	"fmt"
+	"flag"
+)
 // Main function
 func main() {
   
-    fmt.Println("Hello World")
+	// variables declaration  
+    var player_name string  
+    var character_name string
+
+	// flags declaration using flag package
+	flag.StringVar(&player_name, "pname", "test1", "Specify player's name. Required.")
+	flag.StringVar(&character_name, "cname", "test2", "Specify character's name. Required.")
+
+	flag.Parse()  // after declaring flags we need to call it
+
+    fmt.Println("Golang Character Generator!")
+	fmt.Println("Player Name: " + player_name)
+	fmt.Println("Character Name: " + character_name)
+
 }
